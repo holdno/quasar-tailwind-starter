52303f3caf07f5667c447496f9d1a4f11cc58515
holdno <wby@ojbk.io>
2022-02-25 15:39:27 +0800

    menu text color

--- src/layout/MenuItem.vue
+++ src/layout/MenuItem.vue
@@ -4,7 +4,9 @@
     <template v-if="item.children && item.children.length > 0">
       <q-expansion-item
         :label="item.title"
-        header-class="tw-font-medium"
+        :header-class="
+          'tw-font-medium ' + (item.open ? 'tw-text-black' : 'tw-text-gray-600')
+        "
         :header-inset-level="insetLevel"
         :icon="item.icon"
         v-model="item.open"
@@ -19,12 +21,13 @@
         v-model="item.open"
         :inset-level="insetLevel"
         :to="item.router"
+        class="tw-text-gray-600 tw-font-medium"
       >
         <q-item-section avatar>
           <q-icon :name="item.icon" />
         </q-item-section>
 
-        <q-item-section class="tw-font-medium">{{ item.title }}</q-item-section>
+        <q-item-section class="">{{ item.title }}</q-item-section>
       </q-item>
     </template>
   </template>
